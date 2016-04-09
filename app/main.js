48f572f43e617befcec54b4793eddeebe9b5a113
MicYoung <youn462@usc.edu>
2016-04-09 11:47:17 -0700

    no more errors

--- app/main.js
+++ app/main.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import {render} from 'react-dom';
-import Greeter from './Greeter';
+import React, {Component} from 'react'
+import {render} from 'react-dom'
+import config from './config.json'
+import {Router, Route, IndexRoute, hashHistory} from 'react-router'
 
 import './main.css';
 
-render(
-	<Greeter />,
-	document.getElementById('root')
-);
+import Nav from './pages/Nav'
+import HomePage from './pages/HomePage'
+
+render((
+		<Router history={hashHistory}>
+			<Route path='/' component = {Nav}>
+				<IndexRoute component={HomePage}/>
+			</Route>
+		</Router>
+	), document.getElementById('root'));	
