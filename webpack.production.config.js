e51737f5794ca793501ea1c733ec248161524a00
MicYoung <youn462@usc.edu>
2016-04-08 23:06:21 -0700

    production server

---
+++ webpack.production.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: __dirname + "/app/main.js",
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.json$/,
+        loader: "json"
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      }
+    ]
+  }
+
+}
