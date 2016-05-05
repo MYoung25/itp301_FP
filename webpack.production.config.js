15a5d433f5bdb4240510c06ed0f3b0f7f389ea68
Michael Young <michael.young@myactions.org>
2016-05-05 03:17:27 -0400

    fully working, no errors, production in build directory

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -18,6 +18,11 @@ module.exports = {
   },
 
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new ExtractTextPlugin("style.css")
