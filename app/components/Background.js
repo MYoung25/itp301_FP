48c64278f395c44d1faafd647d1b185a744ba74d
Michael Young <michael.young@myactions.org>
2016-05-20 14:53:37 -0700

    added NY Times Animation

--- app/components/Background.js
+++ app/components/Background.js
@@ -6,7 +6,7 @@ class Background extends Component{
 	constructor (props){
 		super(props);
 		if(localStorage.getItem('background_img') !== null){
-			document.querySelector('body').style.background = "url(" + localStorage.getItem("background_img") + ") cover";
+			document.querySelector('body').style.background = "url(" + localStorage.getItem("background_img") + ")";
 		}
 	}
 	render() {
@@ -21,7 +21,6 @@ class Background extends Component{
 		console.log('upload image form works');
 		var upload = document.querySelector('.upload').value
 		console.log(upload);
-		// document.querySelector('body').style.background = "url(" + upload + ") no-repeat fixed";
 	}
 	uploadImageJquery(){
 		$(function () {
@@ -35,9 +34,9 @@ class Background extends Component{
 		});
 
 		function imageIsLoaded(e) {
-	    	document.querySelector('body').style.background = "url('" + e.target.result + "') fixed";
+			console.log(e.target.result);
+	    	document.querySelector('body').style.background = "url('" + e.target.result + "')";
 	    	localStorage.setItem("background_img", e.target.result);
-	    	document.querySelector('body').style.backgroundSize = "200vh";
 		};
 
 	}
--- app/components/News.js
+++ app/components/News.js
@@ -17,6 +17,7 @@ class News extends Component {
 		}
 		this.getNews = this.getNews.bind(this);
 		this.nyTimes = this.nyTimes.bind(this);
+		this.nyTimesApp = this.nyTimesApp.bind(this);
 	}
 	getNews(){
 		var apikey = "a620e8bff37d1173b6e6ed8712b999d4:1:75112088";
@@ -35,6 +36,46 @@ class News extends Component {
 	nyTimes(){
 		window.location.href="http://developer.nytimes.com";
 	}
+	nyTimesApp(obj){
+		var newsAnimation = parseInt(window.getComputedStyle(document.querySelector('.news')).right);
+
+		document.querySelector('.news').style.overflow = 'scroll';
+
+		var animation;
+		if(newsAnimation == -400){
+			animation = setInterval(slideIn, 1);
+		} else {
+			animation = setInterval(slideOut, 1);
+		}
+		
+		function slideIn(){
+			if(newsAnimation != '0px'){
+				newsAnimation = parseInt(newsAnimation);
+				newsAnimation += 5;
+				newsAnimation +='px';
+				document.querySelector('.news').style.right = newsAnimation;
+			} else {
+				document.querySelector('.nytimes-app').style.display = 'none';
+				document.querySelector('.nytimes-close').style.display = 'block';
+				clearInterval(animation);
+			}
+		}
+
+		function slideOut(){
+			if(newsAnimation != '-400px'){
+				newsAnimation = parseInt(newsAnimation);
+				newsAnimation -= 5;
+				newsAnimation += 'px';
+				document.querySelector('.news').style.right = newsAnimation;
+			} else {
+				document.querySelector('.nytimes-app').style.display = 'block';
+				document.querySelector('.nytimes-close').style.display = 'none';
+				document.querySelector('.news').style.overflow = 'visible';
+				clearInterval(animation);
+			}
+		}
+
+	}
 	render(){
 		var articles = [];
 		this.state.newsResult.forEach(function(news){
@@ -42,7 +83,11 @@ class News extends Component {
 		});
 		return(
 			<div className="news">
+
 				<h2 className="nytimes" onClick={this.nyTimes}></h2>
+				<img className="nytimes-app" src="./assets/nyt-app.png" onClick={this.nyTimesApp} />
+				<img className="nytimes-close" src="./assets/close.png" onClick={this.nyTimesApp} />
+
 				<div className="news-snippets">
 					{articles}
 				</div>
